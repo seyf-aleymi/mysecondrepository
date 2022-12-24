13c7a94658b2b711dc5b939c7da769983690de4a
seyf-aleymi <73020155+seyf-aleymi@users.noreply.github.com>
2022-12-24 17:13:20 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Address from './Component/Profile'
+import FullName from './Component/Profile'
+import ProfilePhoto from './Component/Profile'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Component/ProfilePhoto.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+import 'antd/dist/antd.css';
+ 
+class ProfilePhoto extends Component {
+    render (){
+        return (
+
+        <div className="ProfilePhoto">
+          <h3>Component connected</h3>  
+          </div>
+        );
+    }
+
+    }
+export default ProfilePhoto;
