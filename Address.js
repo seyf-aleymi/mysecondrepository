0e5f8efd9e9f5ce8e38e9e81bff637bd49b2efbf
seyf-aleymi <73020155+seyf-aleymi@users.noreply.github.com>
2022-12-24 17:21:24 +0100

    Add files via upload

---
+++ Address.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  const address = 'this is my address';
+
+  return <h1>{Gafsa}</h1>;
+}
+
+export default App;
---
+++ FullName.js
@@ -0,0 +1,7 @@
+import react from 'react';
+function fullName(firstName,lastName) {
+    fullname =("Seyf" , "Aleymi");
+    return 'Seyf Aleymi';
+ }
+
+    export default FullName
